5351450332fe06cf07e8d02afde1af32e5bd7b4e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-17 10:05:29 +0900

    2.3.3演習No.1の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    # 2.3.3演習No.1
+    @micropost = @user.microposts.first
   end
 
   # GET /users/new
